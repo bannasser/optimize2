0c9a1bf98fdd4b515fa77567aa3a936f1c8608c0
bannasser <bannasser@users.noreply.github.com>
2015-01-31 22:36:47 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -496,6 +496,19 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 
 // The following code for sliding background pizzas was pulled from Ilya's demo found at:
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
+<script>
+    function updatePositions() {
+  frame++;
+  window.performance.mark("mark_start_frame");
+
+  var items = document.querySelectorAll('.mover');
+  for (var i = 0; i < items.length; i++) {
+    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+  }
+    }
+    
+  </script>
 
 // Moves the sliding background pizzas based on scroll position
 
