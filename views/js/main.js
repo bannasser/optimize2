8cf0543d80d22b5c60c0738dcfca9ee29ed89ff2
bannasser <bannasser@users.noreply.github.com>
2015-02-02 20:52:35 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -495,12 +495,19 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
   }
   console.log("Average time to generate last 10 frames: " + sum / 10 + "ms");
 }
-
-// The following code for sliding background pizzas was pulled from Ilya's demo found at:
-// https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
-  frame++;
+function updatePositions() {
+   frame++;
   window.performance.mark("mark_start_frame");
 
+var items = document.querySelectorAll('.mover');
+  for (var i = 0; i < items.length; i++) {
+    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+  }
+ }
+// The following code for sliding background pizzas was pulled from Ilya's demo found at:
+// https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
+  
   
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
