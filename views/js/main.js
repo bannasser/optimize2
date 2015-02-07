16b9eabae1f68bd6835ed7af2b1c81a66fb00d02
bannasser <bannasser@users.noreply.github.com>
2015-02-07 18:19:37 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -519,7 +519,7 @@ function updatePositions() {
 }*/
 
 // runs updatePositions on scroll
-/*window.addEventListener('scroll', updatePositions);
+window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
@@ -538,4 +538,4 @@ document.addEventListener('DOMContentLoaded', function() {
   updatePositions();
 });
 
-*/
+
