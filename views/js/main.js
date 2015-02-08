801bb3b505409511304062441c45c430ed91c3b1
bannasser <bannasser@users.noreply.github.com>
2015-02-08 01:27:16 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -521,7 +521,7 @@ function updatePositions() {
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
 
   }
-       var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+       var phase = Math.sin((document.body.scrollTop / 1250) + 1);
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
