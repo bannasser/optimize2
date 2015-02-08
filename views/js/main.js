27a04a7932b1f41a83a2d1ebdcd5a9cedbb837c4
bannasser <bannasser@users.noreply.github.com>
2015-02-08 04:23:31 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -534,10 +534,10 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
     var timesToUpdatePosition = window.performance.getEntriesByName("measure_frame_duration");
     logAverageFrame(timesToUpdatePosition);
   }
-}
+}*/
 
 // runs updatePositions on scroll
-window.addEventListener('scroll', updatePositions);*/
+window.addEventListener('scroll', updatePositions);
 
 //Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
