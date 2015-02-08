fea2b1fd23e909b50d42b05211581edccec6aa97
bannasser <bannasser@users.noreply.github.com>
2015-02-07 20:31:43 -0400

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -482,25 +482,25 @@ console.log("Time to generate pizzas on load: " + timeToGenerate[0].duration + "
 
 // Iterator for number of times the pizzas in the background have scrolled.
 // Used by updatePositions() to decide when to log the average time per frame
-var frame = 0;
+//var frame = 0;
 
 // Logs the average amount of time per 10 frames needed to move the sliding background pizzas on scroll.
-function logAverageFrame(times) {   // times is the array of User Timing measurements from updatePositions()
+/*function logAverageFrame(times) {   // times is the array of User Timing measurements from updatePositions()
   var numberOfEntries = times.length;
   var sum = 0;
   for (var i = numberOfEntries - 1; i > numberOfEntries - 11; i--) {
     sum = sum + times[i].duration;
   }
   console.log("Average time to generate last 10 frames: " + sum / 10 + "ms");
-}
+}*/
 
 // The following code for sliding background pizzas was pulled from Ilya's demo found at:
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
 // Moves the sliding background pizzas based on scroll position
-/*function updatePositions() {
-  frame++;
-  window.performance.mark("mark_start_frame");
+function updatePositions() {
+  /*frame++;
+  window.performance.mark("mark_start_frame");*/
 
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
@@ -508,7 +508,7 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
-  // User Timing API to the rescue again. Seriously, it's worth learning.
+  /* User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
   window.performance.mark("mark_end_frame");
   window.performance.measure("measure_frame_duration", "mark_start_frame", "mark_end_frame");
